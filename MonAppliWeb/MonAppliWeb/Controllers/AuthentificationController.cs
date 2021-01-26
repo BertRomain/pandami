6478c9b9f8cda2b53767f480a287b6d30eaeb284
Yasmine <lyasminem@gmail.com>
2021-01-26 11:36:48 +0100

    Ajout lien vers vues inscription et connection

--- MonAppliWeb/MonAppliWeb/Controllers/AuthentificationController.cs
+++ MonAppliWeb/MonAppliWeb/Controllers/AuthentificationController.cs
@@ -10,12 +10,23 @@ namespace MonAppliWeb.Controllers
     public class AuthentificationController : Controller
     {
         // GET: Connexion
-        public ActionResult Inscription()
+        public ActionResult Inscription(string FNMb, string LNMb, DateTime BDMb, string MailMb, int NumMb, string AdMb, int CityMb, string LogMb, string PWMb)
         {
             //Appelle méthode inscription
             Member NewMember = new Member();
 
+            //Enregistrement du nouveau membre
+            NewMember.CreateMember(FNMb, LNMb, BDMb, MailMb, NumMb, AdMb, CityMb, LogMb, PWMb);
+
             return View();
+        }
+
+        public string IActionInvokerFactory(int ZCMb)
+        {
+            Member NewMember = new Member();
+
+            //Affichage des villes en focntions du code postal saisi
+            return  NewMember.AfficherVilles(ZCMb);
         }
     }
 }
